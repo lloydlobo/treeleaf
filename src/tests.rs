4b7441c0b41158215c0d913fff582cd7bae807aa
lloydlobo <76430758+lloydlobo@users.noreply.github.com>
2023-01-26 18:22:40 +0530

    fix: fix spelling of test case

--- src/tests.rs
+++ src/tests.rs
@@ -19,7 +19,7 @@ fn render_tree_root() {
 
 #[test]
 fn render_tree_with_leaves() {
-    let tree = Tree::new("foo").with_leaves([Tree::new("bar").with_leaves(["baze"])]);
+    let tree = Tree::new("foo").with_leaves([Tree::new("bar").with_leaves(["baz"])]);
     assert_eq!(
         format!("{}", tree),
         r#"foo
